f02f8cd81c4fa69807d1dc9cc4eba9e86c913fce
Robson Narcizo <rob.narcizo.27@gmail.com>
2023-01-24 15:38:51 -0300

    feat: Implementação Controladores

--- src/routes/card.routes.js
+++ src/routes/card.routes.js
@@ -1,7 +1,8 @@
 const express = require('express');
+const { cardController } = require('../controllers');
 const router = express.Router();
 
-router.get('/', (req, res) => res.status(200).json({ message: 'Todos os cards' }));
-router.post('/', (req, res) => res.status(200).json({ message: 'Cadastra um card' }));
+router.get('/', cardController.getAllCards);
+router.post('/', cardController.createCard);
 
 module.exports = router;

a8be61680f4b95f36885fb715305a7e1ea82e4f3
Robson Narcizo <rob.narcizo.27@gmail.com>
2023-01-25 11:26:52 -0300

    Recuperação de dados no formato JSON

--- src/app.js
+++ src/app.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const app = express();
 
+app.use(express.json());
+
 app.use('/cards', require('./routes/card.routes'));
 
 module.exports = app;
--- src/controllers/card.controller.js
+++ src/controllers/card.controller.js
@@ -1,12 +1,12 @@
 const { cardModel } = require('../models');
-const connection = require('../models/connection');
 
 const getAllCards = async (req, res) => {
     const [ result ] = await cardModel.findAll(); 
     res.status(200).json(result);
 };
 
-const createCard = (req, res) => res.status(200).json({ message: 'Cadastra um card' });
+const createCard = async (req, res) => res.status(200).json(req.body);
+
 
 const getCardsById = async (req, res) => {
     const { id } = req.params;
